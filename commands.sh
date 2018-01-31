20d21efd6ae8594832c6f8d8beee5d413ca3b938
Guido Julian Buchely Moreno <guido.buchely@globant.com>
2018-01-31 15:40:12 -0500

    Add docker commands for cleanig

--- commands.sh
+++ commands.sh
@@ -1,6 +1,6 @@
 cd application/
 gradle clean build -x test
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker rmi akkaspring_consumer1 akkaspring_worker1 akkaspring_worker2 akkaspring_publisher1 -f
+docker stop $(docker ps -a -f "name=akkaspring_" -q)
+docker rm $(docker ps -a -f "name=akkaspring_" -q)
+docker rmi $(docker images "akka*" -q) -f
 docker images
